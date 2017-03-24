8e25348628983170e176c83c5c2bac3537f845b1
BadoDavid <bado.david@sch.bme.hu>
2017-03-24 23:21:38 +0100

    tamas work
    without wall and network implementation

--- hf_brszta/src/szoftechtutor/Main.java
+++ hf_brszta/src/szoftechtutor/Main.java
@@ -5,6 +5,14 @@
 
 package szoftechtutor;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
+
+
 /**
  *
  * @author Predi
@@ -16,8 +24,35 @@ public class Main {
 	 *            the command line arguments
 	 */
 	public static void main(String[] args) {
+		/*
+		 * Java gyakorlaton bemutatott mintaprogram main-jének belseje
+		 * ehhez tartozik még a GUI.java, a Control.java 
+		 * és a Network.java, de ez még nincs implementálva
+		 * 
 		Control c = new Control();
 		GUI g = new GUI(c);
 		c.setGUI(g);
+		*/
+		
+		Dimension d = new Dimension(1024,768); // Ablak fix méretének beállítása
+
+		
+		JFrame window = new JFrame(); // Ablak létrehozása
+		window.setTitle("Faltoro"); // Ablak neve
+		window.setSize(d); // Ablak mérete
+		window.setMaximumSize(d); // Ablak ne legyen átméretezhetõ
+		window.setMinimumSize(d);
+        window.setVisible(true); // Ablak láthatóvá tétele
+        window.setLocationRelativeTo(null); // Ablak elhelyezése a képeryõ közepén
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // memória takarítás bezáráskor
+        
+        faltoro faltoro = new faltoro(); // Új faltoro létrehozása
+        window.add(faltoro); // hozzáadása az ablakhoz
+        
+        window.addMouseMotionListener(faltoro); // egér mûködtetése
+        
+        Timer timer = new Timer(4, faltoro); // ciklikus lefutás
+        timer.start();
+        
 	}
 }
---
+++ hf_brszta/src/szoftechtutor/faltoro.java
@@ -0,0 +1,144 @@
+package szoftechtutor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
+public class faltoro extends JComponent implements ActionListener, MouseMotionListener, KeyListener  {
+	
+	//public static Dimension dimension = new Dimension(1024,768); // Ablak fix méretének beállítása
+	
+	// Változók inicializálása 
+	public int state = 0;
+	public int labda_sebesseg_x = 2;
+	public int labda_sebesseg_y = 2;
+	public int labda_poz_x = 150;
+	public int labda_poz_y = 300;
+	public int labda_size = 24;
+	public int uto_size_x = 150;
+	public int uto_size_y = 20;
+	public int uto_poz = 0;
+	
+	@Override
+    protected void paintComponent(Graphics faltoro) {
+		// labda
+		faltoro.setColor(Color.cyan); // labda színének beállítása
+        faltoro.fillOval(labda_poz_x, labda_poz_y, labda_size, labda_size);
+        
+        // ütõ beállításai
+        faltoro.setColor(Color.blue); // ütõ színének beállítása
+        if(uto_poz>852){ // ütõ mozgásának korlátozása
+        	uto_poz=852;
+        }
+        faltoro.fillRect(uto_poz, 668, uto_size_x, uto_size_y);
+        
+        // téglák
+        // j-> sorok száma    i-> oszlopok száma
+        /*for(int j = 0; j<5; ++j){
+        	for(int i = 0; i<10; ++i) {
+        		faltoro.setColor(Color.black);
+        		faltoro.fillRect(100*i+20,30*j+5,40,20);
+        	}
+        }*/
+	}
+
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		labda_poz_x = labda_poz_x + labda_sebesseg_x;
+		labda_poz_y = labda_poz_y + labda_sebesseg_y;
+		
+		// leesett a labda
+		if(labda_poz_y > 680){
+			// game over
+		}
+		
+		// ütõt ért a labda
+		if((labda_poz_y >= 644) && (labda_poz_y <= 646) && (labda_poz_x >= (uto_poz-14)) && (labda_poz_x <= (uto_poz+uto_size_x+14))){
+						
+			// ha az ütõ jobb szélét érte a labda
+			if(((labda_poz_x - uto_poz) >= 110)){
+				labda_sebesseg_y = -1;
+				labda_sebesseg_x = 3;	
+			}
+			
+			// ha az ütõ bal szélét érte a labda
+			if((labda_poz_x - uto_poz) <= 35){
+				labda_sebesseg_y = -1;
+				labda_sebesseg_x = -3;
+			}
+			
+			// ha az ütõ közepét érte a labda
+			if((((labda_poz_x - uto_poz) > 35)) && ((labda_poz_x - uto_poz) < 110)){
+				int elojel_x = labda_sebesseg_x < 0 ? -1 : 1; // elõjel kinyerése
+				int elojel_y = labda_sebesseg_y < 0 ? 1 : -1; // elõjel változtatása
+				labda_sebesseg_y = elojel_y*2;
+				labda_sebesseg_x = elojel_x*2;
+			}
+						
+			labda_poz_y = labda_poz_y - 3;  // hogy azonnal eltávolodjon az ütõtõl
+		}
+		
+		// bal fal
+		if(labda_poz_x < 10){
+			labda_sebesseg_x = -1*labda_sebesseg_x;
+		}
+		
+		// jobb fal
+		if(labda_poz_x > 970){
+			labda_sebesseg_x = -1*labda_sebesseg_x;
+		}
+		
+		// tetõ
+		if(labda_poz_y < 10){
+			labda_sebesseg_y = -1*labda_sebesseg_y;
+		}
+		
+		repaint(); // képernyõ újrarajzolása
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		uto_poz = arg0.getX(); // egér x pozíciójának beolvasása
+	
+		repaint(); // képernyõ újrarajzolása
+	}
+	
+}
