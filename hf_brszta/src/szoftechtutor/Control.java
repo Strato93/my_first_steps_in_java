f683776e23d92975bdb26d86fad515c580278661
Dávid Badó <bado.david@sch.bme.hu>
2017-03-24 22:32:18 +0100

    sdfsldknflsdnf

--- hf_brszta/src/szoftechtutor/Control.java
+++ hf_brszta/src/szoftechtutor/Control.java
@@ -24,29 +24,15 @@ class Control {
 	}
 
 	void startServer() {
-		if (net != null)
-			net.disconnect();
-		net = new SerialServer(this);
-		net.connect("localhost");
 	}
 
 	void startClient() {
-		if (net != null)
-			net.disconnect();
-		net = new SerialClient(this);
-		net.connect("localhost");
 	}
 
 	void sendClick(Point p) {
-		// gui.addPoint(p); //for drawing locally
-		if (net == null)
-			return;
-		net.send(p);
+		gui.addPoint(p); // for drawing locally
 	}
 
 	void clickReceived(Point p) {
-		if (gui == null)
-			return;
-		gui.addPoint(p);
 	}
 }
