8d4c8d35dc0e4756445b257b35baeb99b2fa29a0
badbeloved <bado.david@sch.bme.hu>
2017-03-29 10:42:52 +0200

    marton work

--- hf_brszta/src/szoftechtutor/faltoro.java
+++ hf_brszta/src/szoftechtutor/faltoro.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -14,20 +15,21 @@ import javax.swing.JFrame;
 import javax.swing.Timer;
 import javax.swing.WindowConstants;
 
-public class faltoro extends JComponent implements ActionListener, MouseMotionListener, KeyListener  {
+public class faltoro extends JComponent implements ActionListener, MouseListener, MouseMotionListener, KeyListener   {
 	
 	//public static Dimension dimension = new Dimension(1024,768); // Ablak fix méretének beállítása
 	
 	// Változók inicializálása 
 	public int state = 0;
-	public int labda_sebesseg_x = 2;
-	public int labda_sebesseg_y = 2;
-	public int labda_poz_x = 150;
-	public int labda_poz_y = 300;
+	public int labda_sebesseg_x = 0;
+	public int labda_sebesseg_y = 0;
+	public int labda_poz_x = 480;
+	public int labda_poz_y = 640;
 	public int labda_size = 24;
 	public int uto_size_x = 150;
 	public int uto_size_y = 20;
-	public int uto_poz = 0;
+	public int uto_poz = 426;
+	public boolean click = true;
 	
 	@Override
     protected void paintComponent(Graphics faltoro) {
@@ -41,15 +43,14 @@ public class faltoro extends JComponent implements ActionListener, MouseMotionLi
         	uto_poz=852;
         }
         faltoro.fillRect(uto_poz, 668, uto_size_x, uto_size_y);
-        
         // téglák
         // j-> sorok száma    i-> oszlopok száma
-        /*for(int j = 0; j<5; ++j){
+        for(int j = 0; j<5; ++j){
         	for(int i = 0; i<10; ++i) {
         		faltoro.setColor(Color.black);
         		faltoro.fillRect(100*i+20,30*j+5,40,20);
         	}
-        }*/
+        }
 	}
 
 	
@@ -63,7 +64,12 @@ public class faltoro extends JComponent implements ActionListener, MouseMotionLi
 		
 		// leesett a labda
 		if(labda_poz_y > 680){
-			// game over
+			uto_poz = 426;
+			labda_poz_x = 480;
+			labda_poz_y = 640;
+			labda_sebesseg_y = 0;
+			labda_sebesseg_x = 0;
+			click=true;// game over
 		}
 		
 		// ütõt ért a labda
@@ -129,9 +135,14 @@ public class faltoro extends JComponent implements ActionListener, MouseMotionLi
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+	public void mouseClicked(MouseEvent e) {
 		
+		if (click==true){
+		labda_sebesseg_x = 2;
+		labda_sebesseg_y = -2;
+		repaint();
+		click=false;
+		}
 	}
 
 	@Override
@@ -139,6 +150,36 @@ public class faltoro extends JComponent implements ActionListener, MouseMotionLi
 		uto_poz = arg0.getX(); // egér x pozíciójának beolvasása
 	
 		repaint(); // képernyõ újrarajzolása
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
